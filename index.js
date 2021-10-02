9d897d22936a0d7ba37191382f6bab68e44d1e87
tizano <ma.scarlatella@gmail.com>
2021-10-02 22:40:16 +0200

    fix CORS

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ const cardRoutes = require('./routes/card.controller');
 
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const allowedOrigins = ['http://localhost:3000', 'https://pokemon.mathieuscarlatella.fr'];
 app.use(cors({
-  origin: ['http://localhost*', 'https://pokemon.mathieuscarlatella.fr']
-  // origin: '*'
+  origin: (origin, callback) => {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin)
+      return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
+
 app.use('/cards', cardRoutes);
 
 // app.get('/', (req, res) => {
